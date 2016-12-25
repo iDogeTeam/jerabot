37e1243464d9d21af3280210869bd3e7c7ed94c8
NeverBehave <luoxinhao77@gmail.com>
2016-12-25 23:11:20 +0800

    remove revert

--- src/Feng/JeraBot/Commands/GiftcodeCommand.php
+++ src/Feng/JeraBot/Commands/GiftcodeCommand.php
@@ -47,35 +47,6 @@ class GiftcodeCommand extends Command
         
     }
 
-    /**
-     * utf8字符转换成Unicode字符
-     * @param  [type] $utf8_str Utf-8字符
-     * @return [type]           Unicode字符
-     */
-    function utf8_str_to_unicode($utf8_str) {
-        $unicode = 0;
-        $unicode = (ord($utf8_str[0]) & 0x1F) << 12;
-        $unicode |= (ord($utf8_str[1]) & 0x3F) << 6;
-        $unicode |= (ord($utf8_str[2]) & 0x3F);
-        return dechex($unicode);
-    }
-
-    /**
-     * Unicode字符转换成utf8字符
-     * @param  [type] $unicode_str Unicode字符
-     * @return [type]              Utf-8字符
-     */
-    function unicode_to_utf8($unicode_str) {
-        $utf8_str = '';
-        $code = intval(hexdec($unicode_str));
-        //这里注意转换出来的code一定得是整形，这样才会正确的按位操作
-        $ord_1 = decbin(0xe0 | ($code >> 12));
-        $ord_2 = decbin(0x80 | (($code >> 6) & 0x3f));
-        $ord_3 = decbin(0x80 | ($code & 0x3f));
-        $utf8_str = chr(bindec($ord_1)) . chr(bindec($ord_2)) . chr(bindec($ord_3));
-        return $utf8_str;
-    }
-
     public function handle($arguments)
     {
         $bridge = new PanelBridge();
@@ -88,21 +59,19 @@ class GiftcodeCommand extends Command
             return;
         }
         $this->logger->addInfo( "尝试使用礼品码：Doge {$user->id}，Name:{$user->user_name},TGID:{$user->telegram_id}, username: @{$tuser} 参数: {$this->getOption(0)}");
+
         if ( empty( $this->getOption( 0 ) ) ) {
             $this->triggerCommand( $this->name, "-help" );
             return;
         }
 
-        $code = $bridge->getGiftCode($this->utf8_str_to_unicode($this->getOption(0)));
+        $code = $bridge->getGiftCode($this->getOption(0));
         if ($code->code == null) {
-            $code = $bridge->getGiftCode($this->getOption(0));
-            if ($code->code == null) {
                 $this->replyWithMessage(array(
                     "text" => "礼品码不存在!"
                 ));
                 return;
             }
-        }
 
         $get_date = $code->expire_at;
 
@@ -187,7 +156,7 @@ class GiftcodeCommand extends Command
             ));
             return;
         }
-        $response = "邀请码应用成功!";
+        $response = "礼品码应用成功!";
         switch ($code_type){
             case 0:
                 $response.="\r\n_{$traffic}_GB流量已经生效。" ;
